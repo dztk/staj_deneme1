b859ad147bdce3400d76357cd8987af8669f8d3b
dztk <deniztprk02@gmail.com>
2025-07-31 21:50:44 +0300

    Initial commit

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace staj_deneme1.Models
+{
+    
+        public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<sefer> Seferler { get; set; }
+    }
+
+    
+}
---
+++ Models/sefer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace staj_deneme1.Models
+{
+    public class sefer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string SeferNo { get; set; }
+
+        [Required]
+        public string Kalkis { get; set; }
+
+        [Required]
+        public string Varis { get; set; }
+
+        [Required]
+        public DateTime TarihSaat { get; set; }
+    }
+}
+
+
+
+
